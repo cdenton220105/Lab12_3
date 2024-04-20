ca732b375b94d9c05be0705da51d7d06387bcc25
Claire Denton <cdenton@unomaha.edu>
2024-04-19 21:33:40 -0500

    Test commit 1

--- television.py
+++ television.py
@@ -6,46 +6,78 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Function initializes Television objects
+        Instance variables for power, mute, volume, and channel are initialized
+        """
         self.__status = False
         self.__muted = False
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Function negates current power status when called, turning on or off the TV
+        """
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Function negates current mute status when called
+        """
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Function lowers channel by 1
+        If TV is at maximum channel, function will set channel to minimum channel
+        """
         if self.__status:
             if self.__channel == self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
             else:
                 self.__channel += 1
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Function lowers channel by 1
+        If TV is at minimum channel, function will set channel to maximum channel
+        """
         if self.__status:
             if self.__channel == self.MIN_CHANNEL:
                 self.__channel = self.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Function lowers volume by 1
+        If maximum volume is reached, function will leave TV at maximum volume
+        Function removes mute on TV when called
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
             if not(self.__volume == self.MAX_VOLUME):
                 self.__volume += 1
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Function lowers volume by 1
+        If minimum volume is reached, function will leave TV at minimum volume
+        Function removes mute on TV when called
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
             if not(self.__volume == self.MIN_VOLUME):
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Function changes the default str function to print the power status, current channel, and current volume
+        :return: String
+        """
         if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {0}'
         else:
